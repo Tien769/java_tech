8e1e37d998dbc1745edfef180ac6d971a5fbb18d
Isaac <minimaleffort769@gmail.com>
2020-06-14 11:26:22 +0700

    minor fix to previous merge

--- src/main/java/managedbean/ListPageBean.java
+++ src/main/java/managedbean/ListPageBean.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import ejb.DatabaseService;
